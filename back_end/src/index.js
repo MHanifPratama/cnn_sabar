948499a98746e7f6531bd3e397636c2b26e71c47
M Hanif Pratama <mhanifp27@gmail.com>
2023-12-02 00:06:23 +0700

    secret key env

--- back_end/src/index.js
+++ back_end/src/index.js
@@ -4,10 +4,10 @@ const PORT = process.env.PORT
 const express = require('express');
 const middlewareLog = require('./middleware/logs.js');
 const middlewareValidation = require('./middleware/validation_token.js');
+
 const dosenRoutes = require('./routes/dosen.js');
 const mahasiswaRoutes = require('./routes/mahasiswa.js');
 const userRoutes = require('./routes/user.js');
-
 
 const app = express();
 app.use(middlewareLog);
@@ -21,11 +21,13 @@ app.use(middlewareValidation);
 app.use("/api/v1/dosen", dosenRoutes)
 app.use("/api/v1/mahasiswa", mahasiswaRoutes)
 
+
 app.use("/",(req,res)=>{
     res.json({
         message: "Hello World"
     })
 })
+
 
 app.listen(PORT,()=> {
     console.log("Server is running on port ",PORT);
