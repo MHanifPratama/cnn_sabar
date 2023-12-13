e0020a3ec08ac3740df792943ae05cd8cb13d2c2
Tania <nathania.santa21@students.unila.ac.id>
2023-12-13 22:04:34 +0700

    update peminat

--- back_end/src/routes/peminat.js
+++ back_end/src/routes/peminat.js
@@ -3,6 +3,7 @@ const PeminatController = require("../controller/peminat.js");
 const routes = express.Router();
 
 routes.get("/", PeminatController.getAllPeminat);
+routes.get("/count", PeminatController.countPeminat);
 routes.post("/", PeminatController.createNewPeminat);
 routes.get("/:id", PeminatController.detailPeminat);
 routes.patch("/:id", PeminatController.updatePeminat);
